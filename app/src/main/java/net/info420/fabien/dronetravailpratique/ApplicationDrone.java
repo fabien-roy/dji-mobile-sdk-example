d06680a4d7bd1718b68805ec4cea651065ec02eb
Fabien Roy <exilednarwal28@gmail.com>
2017-02-10 13:38:03 -0500

    Prepared main app

---
+++ app/src/main/java/net/info420/fabien/dronetravailpratique/ApplicationDrone.java
@@ -0,0 +1,18 @@
+package net.info420.fabien.dronetravailpratique;
+
+import android.app.Application;
+
+/**
+ * Created by fabien on 17-02-10.
+ */
+
+public class ApplicationDrone extends Application {
+  // Parameters used in the whole application
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Instanciation used in the whole application
+  }
+}
