0f1003acf12efa8cb46c5654ca9336eae8a412c0
Fabien Roy <exilednarwal28@gmail.com>
2017-02-17 13:22:13 -0500

    Can now connect to drone and display information

--- app/src/main/java/net/info420/fabien/dronetravailpratique/common/ContentMain.java
+++ app/src/main/java/net/info420/fabien/dronetravailpratique/common/ContentMain.java
@@ -1,12 +1,16 @@
 package net.info420.fabien.dronetravailpratique.common;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import net.info420.fabien.dronetravailpratique.R;
 
@@ -18,10 +22,12 @@ import dji.thirdparty.eventbus.EventBus;
  * Created by fabien on 17-02-14.
  */
 
-public class ContentMain extends RelativeLayout {
+public class ContentMain extends RelativeLayout implements DJIBaseProduct.DJIVersionCallback {
 
   public static final String TAG = ContentMain.class.getName();
 
+  private TextView mTextProduct;
+  private TextView mTextModelAvailable;
   private TextView mTextConnectionStatus;
   private Button mBtnOpen;
 
@@ -40,6 +46,8 @@ public class ContentMain extends RelativeLayout {
   private void initUI() {
     Log.d(TAG, "initUI()");
 
+    mTextModelAvailable = (TextView) findViewById(R.id.text_model_available);
+    mTextProduct = (TextView) findViewById(R.id.text_product_info);
     mTextConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
     mBtnOpen = (Button) findViewById(R.id.btn_open);
 
@@ -52,18 +60,68 @@ public class ContentMain extends RelativeLayout {
     });
   }
 
+  @Override
+  protected void onAttachedToWindow() {
+    Log.d(TAG, "onAttachedToWindow()");
+    refreshSDKRelativeUI();
+    IntentFilter filter = new IntentFilter();
+    filter.addAction(ApplicationDrone.FLAG_CONNECTION_CHANGE);
+    getContext().registerReceiver(mReceiver, filter);
+    super.onAttachedToWindow();
+  }
+
+  private void updateVersion() {
+    String version = null;
+    if(mProduct != null) {
+      version = mProduct.getFirmwarePackageVersion();
+    }
+
+    if(version == null) {
+      mTextModelAvailable.setText("N/A");
+    } else {
+      mTextModelAvailable.setText(version);
+    }
+  }
+
+  @Override
+  public void onProductVersionChange(String oldVersion, String newVersion) {
+    updateVersion();
+  }
+
+
+  protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      Log.d(TAG, "BroadcastReceiver : onReceive()");
+      refreshSDKRelativeUI();
+    }
+
+  };
+
+
   // Vérifie si le drone est connecté et active l'interface necéssaire
   private void refreshSDKRelativeUI() {
     mProduct = ApplicationDrone.getProductInstance();
+    Toast.makeText(getContext(), "mProduct: " + (mProduct == null? "null" : "unnull"), Toast.LENGTH_LONG).show();
     Log.d(TAG, "mProduct: " + (mProduct == null? "null" : "unnull") );
     if (null != mProduct && mProduct.isConnected()) {
       mBtnOpen.setEnabled(true);
 
       String str = mProduct instanceof DJIAircraft ? "DJIAircraft" : "DJIHandHeld";
-      mTextConnectionStatus.setText("Status: " + str + " connected");
+      mTextConnectionStatus.setText("Statut : " + str + " connecté");
+      mProduct.setDJIVersionCallback(this);
+      updateVersion();
+
+      if (null != mProduct.getModel()) {
+        mTextProduct.setText("" + mProduct.getModel().getDisplayName());
+      } else {
+        mTextProduct.setText(R.string.product_information);
+      }
     } else {
       mBtnOpen.setEnabled(false);
 
+      mTextProduct.setText(R.string.product_information);
       mTextConnectionStatus.setText(R.string.connection_loose);
     }
   }
